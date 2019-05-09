3a676641be3bead12e6666242c6e63654c60cb04
Michael Milton <ttmigueltt@gmail.com>
2019-05-09 15:48:54 +1000

    Bump version for release

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ class BuildHtmlDocs(distutils.cmd.Command):
 
 setup(
     name='pandas_schema',
-    version='0.3.3',
+    version='0.3.4',
     description='A validation library for Pandas data frames using user-friendly schemas',
     long_description=long_description,
     url='https://github.com/TMiguelT/PandasSchema',
